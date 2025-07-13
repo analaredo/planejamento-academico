3aeac077df01ea4b390fe00dd8f8fb0b5ecc1041
Miguel <migueltrrds@gmail.com>
2025-07-13 15:08:40 -0300

    feat(validation): ValidadorCreditosMinimos e ValidadorSimples
    - Implementação de um validador de pre-requisitos
    - Implementação de um validador de creditos minimos no periodo

--- src/main/java/br/ufjf/planejamento/validacao/ValidadorCreditosMinimos.java
+++ src/main/java/br/ufjf/planejamento/validacao/ValidadorCreditosMinimos.java
@@ -1,20 +1,21 @@
 package br.ufjf.planejamento.validacao;
 
-import br.ufjf.planejamento.modelo.Disciplina;
-
-import java.util.List;
-
 import br.ufjf.planejamento.modelo.Aluno;
 
-public class ValidadorCreditosMinimos implements ValidadorPreRequisito{
-    private int creditosMinimos;
+public class ValidadorCreditosMinimos implements ValidadorPreRequisito {
+    private final int creditosMinimos;
 
-    public ValidadorCreditosMinimos (int creditos){
+    public ValidadorCreditosMinimos(int creditos) {
         this.creditosMinimos = creditos;
     }
 
     @Override
-    public boolean validar(Aluno aluno){
-        //Falta a implementação de créditos!
+    public boolean validar(Aluno aluno) {
+        return aluno.getCreditosConcluidos() >= this.creditosMinimos;
+    }
+
+    @Override
+    public String getMensagemErro() {
+        return "É necessário ter no mínimo " + creditosMinimos + " créditos concluídos.";
     }
 }
