7048dc205b7c7f87f61169f4c3a68e7f43fac72e
Miguel <migueltrrds@gmail.com>
2025-07-13 15:07:41 -0300

    refactor(validation): Mudança na forma como é mostrado a mensagem

--- src/main/java/br/ufjf/planejamento/validacao/ValidadorLogicoOR.java
+++ src/main/java/br/ufjf/planejamento/validacao/ValidadorLogicoOR.java
@@ -3,23 +3,25 @@ package br.ufjf.planejamento.validacao;
 import br.ufjf.planejamento.modelo.Aluno;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ValidadorLogicoOR implements ValidadorPreRequisito {
     private final List<ValidadorPreRequisito> validadores;
-    
+
     public ValidadorLogicoOR(ValidadorPreRequisito... validadores) {
         this.validadores = Arrays.asList(validadores);
     }
-    
+
     @Override
     public boolean validar(Aluno aluno) {
         return validadores.stream().anyMatch(v -> v.validar(aluno));
     }
-    
+
     @Override
     public String getMensagemErro() {
-        StringBuilder sb = new StringBuilder("Pelo menos um destes pré-requisitos deve ser atendido:");
-        validadores.forEach(v -> sb.append("\n- ").append(v.getMensagemErro()));
-        return sb.toString();
+        String opcoes = validadores.stream()
+                .map(ValidadorPreRequisito::getMensagemErro)
+                .collect(Collectors.joining(" OU "));
+        return "Pelo menos um dos seguintes pré-requisitos deve ser atendido: " + opcoes;
     }
 }
